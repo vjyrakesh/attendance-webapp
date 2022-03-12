18af9bdcc76ca050cd071a38acc78a8e18233f35
Rakesh Kasibhatla <vjyrakesh@gmail.com>
2022-03-12 15:06:13 +0530

    Use Formik for editing

--- src/component/EditStudentComponent.jsx
+++ src/component/EditStudentComponent.jsx
@@ -9,8 +9,7 @@ export default function EditStudentComponent(props) {
     const params = useParams()
     const [student, setStudent] = useState({firstName:'',lastName:'',username:'',standard:''})
     const [standards, setStandards] = useState([])
-    var selectedStd = ''
-
+    
     useEffect(() => {
         console.log("inside fetch student hook")
         StandardService.getAllStandards()
@@ -26,59 +25,56 @@ export default function EditStudentComponent(props) {
         
     },[params.id])
 
-    function handleOnSubmit(event) {
-        event.preventDefault()
-        console.log(student)
-        StudentService.addStudentToStandard(student.id, student.standard)
+    
+    function handleOnSubmit(values) {
+        console.log(values)
+        StudentService.addStudentToStandard(student.id, values.standard)
         .then(response => {
             props.history.push("/students")
         })
     }
 
-    function handleInputChange(event) {
-        const name = event.target.name
-        setStudent(() => ({
-            ...student,
-            [name]: event.target.value,
-        }))
-    }
-
+    
     return (
         <div className="container p-3 col-6">
             <h3>Edit Student</h3>
-                <form onSubmit={handleOnSubmit}>
-                    <div>
-                        <label htmlFor='firstName' className="form-label">First Name</label>
-                        <input name='firstName' type='text' className='form-control' onChange={handleInputChange} value={student.firstName}/>
-                    </div><br/>
-                    <div>
-                        <label htmlFor='lastName' className="form-label">Last Name</label>
-                        <input name='lastName' type='text' className='form-control' onChange={handleInputChange} value={student.lastName}/>
-                    </div><br/>
-                    <div>
-                        <label htmlFor='username' className="form-label">Username</label>
-                        <input name='username' type='text' className='form-control' defaultValue={student.username}/>
-                    </div><br/>
-                    <div>
-                        <label htmlFor='stdName' className='form-label'>Standard</label>
+                <Formik onSubmit={handleOnSubmit} initialValues={{firstName:student.firstName, lastName:student.lastName,username:student.username,standard:student.standard}} enableReinitialize={true}>
+
+                    <Form >
+                        <div>
+                            <label htmlFor='firstName' className="form-label">First Name</label>
+                            <Field name='firstName' type='text' className='form-control'/>
+                        </div><br/>
+                        <div>
+                            <label htmlFor='lastName' className="form-label">Last Name</label>
+                            <Field name='lastName' type='text' className='form-control'/>
+                        </div><br/>
+                        <div>
+                            <label htmlFor='username' className="form-label">Username</label>
+                            <Field name='username' type='text' className='form-control'/>
+                        </div><br/>
+                        <div>
+                            <label htmlFor='stdName' className='form-label'>Standard</label>
+                            <div className='row'>
+                                <div className='col-2'>
+                                    <Field name='standard' className='form-select' as='select'>
+                                        <option value='None'>Select</option>
+                                        
+                                        {
+                                        standards.map(standard => <option value={standard.name} key={standard.id}>{standard.name}</option>)
+                                    }
+                                    </Field>
+                                </div>
+                            </div>
+                        </div><br/>
                         <div className='row'>
-                            <div className='col-2'>
-                                <select name='standard' className='form-select' onChange={handleInputChange} value={student.standard}>
-                                    <option value='None'>Select</option>
-                                    
-                                    {
-                                    standards.map(standard => <option value={standard.name} key={standard.id}>{standard.name}</option>)
-                                }
-                                </select>
+                            <div className='col-1'>
+                                <button type='submit' className='btn btn-success mx-3'>Submit</button>
                             </div>
                         </div>
-                    </div><br/>
-                    <div className='row'>
-                        <div className='col-1'>
-                            <button type='submit' className='btn btn-success mx-3'>Submit</button>
-                        </div>
-                    </div>
-                </form>
+                    </Form>
+                </Formik>
+                
             
         </div>
     )
